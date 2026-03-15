6ed0460f4bf15fc72471638ba577bf6bb27f9a7a
Uno6I <uno6iv@gmail.com>
2026-03-14 19:46:14 -0700

    Update cardputer-servo-server.py

--- cardputer-servo-server.py
+++ cardputer-servo-server.py
@@ -5,7 +5,7 @@ from gpiozero import AngularServo
 from time import sleep
 
 # --- SERVO SETUP ---
-servo = AngularServo(12, min_angle=0, max_angle=180)  # change pin if needed
+servo = AngularServo(13, min_angle=0, max_angle=180)  # change pin if needed
 servo_state = False
 
 
